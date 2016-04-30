06fac4351e8d68682f77145ea8315f9e70205bf8
Jonas Helfer <helfer@meteor.com>
2016-04-30 08:51:44 -0700

    put placeholder for createDecorator and remove existing

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-class Doc {
-  locations = [ 'TYPE', 'FIELD', 'ARG' ];
-  function apply({ description }){
-    return function(decoratedThing) {
-      decoratedThing.description = description;
-    }
-  }
+function createDecorator(decoratorObj){
+  throw new Error('Not implemented yet');
 }
-
-class Log {
-  // TODO: should also apply to type and schema
-  locations = [ 'FIELD' ];
-
-  function constructor({ logger }){
-    // TODO: allow providing more arguments for what to actually log
-    this.logger = logger; 
-  }
-
-  function apply({ prefix }) {
-    return function( wrappedThing ){ 
-      const resolver = wrappedThing.resolve;
-      wrappedThing.resolve = (o, a, c, info){
-        this.logger.log(prefix, info.fieldName, new Date());
-        return resolver(o, a, c, i);
-      }
-    }
-  }
-}
-
-export { Doc, Log };
+export { createDecorator };
